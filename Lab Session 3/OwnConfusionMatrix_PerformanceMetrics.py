dad31d4c5492184e413e913109f6561e514e4dc8
G.S.Akshaya <syamalambaakshaya@gmail.com>
2026-02-01 22:41:35 +0530

    Update OwnConfusionMatrix_PerformanceMetrics.py

--- Lab Session 3/OwnConfusionMatrix_PerformanceMetrics.py
+++ Lab Session 3/OwnConfusionMatrix_PerformanceMetrics.py
@@ -1,3 +1,16 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+df=pd.read_excel("Lab Session Data.xlsx",sheet_name="IRCTC_Stock_Price")
+X=df[["Open","High","Low","Volume"]].to_numpy()
+y=(df["Close"].shift(-1)>df["Close"]).astype(int).to_numpy()
+X=X[:-1]
+y=y[:-1]
+x1,x2,y1,y2=train_test_split(X,y,test_size=0.3)
+n=KNeighborsClassifier(n_neighbors=3)
+n.fit(x1,y1)
+yp=n.predict(x2)
 def cm(y,t):
     tp=sum((y==1)&(t==1))
     tn=sum((y==0)&(t==0))
