bbfa049fd009a22318af297f56a15e60b7938104
G.S.Akshaya <syamalambaakshaya@gmail.com>
2026-02-01 22:30:06 +0530

    Update Minkowski.py

--- Lab Session 3/Minkowski.py
+++ Lab Session 3/Minkowski.py
@@ -1,12 +1,14 @@
-import numpy as np
+import pandas as pd
 import matplotlib.pyplot as p
-def md(a,b,pv):
-    return sum(abs(a[i]-b[i])**pv for i in range(len(a)))**(1/pv)
+df=pd.read_excel("Lab Session Data.xlsx",sheet_name="IRCTC_Stock_Price")
+X=df[["Open","High","Low","Volume"]].to_numpy()
 a=X[0]
 b=X[1]
-d=[]
+def md(a,b,pv):
+    return sum(abs(a[i]-b[i])**pv for i in range(len(a)))**(1/pv)
+r=[]
 k=range(1,11)
 for i in k:
-    d.append(md(a,b,i))
-p.plot(k,d)
+    r.append(md(a,b,i))
+p.plot(k,r)
 p.show()
