96f7977b1d72e9da100618adcf646d28895b8817
G.S.Akshaya <syamalambaakshaya@gmail.com>
2026-02-01 22:13:00 +0530

    Update DotProduct.py

--- Lab Session 3/DotProduct.py
+++ Lab Session 3/DotProduct.py
@@ -3,9 +3,12 @@ def d(a,b):
     return sum(a[i]*b[i] for i in range(len(a)))
 def l(a):
     return (sum(i*i for i in a))**0.5
-a=np.array([1,2,3])
-b=np.array([4,5,6])
+df=pd.read_excel("Lab Session Data.xlsx",sheet_name="IRCTC_Stock_Price")
+X=df[["Open","High","Low","Volume"]].to_numpy()
+a=X[0]
+b=X[1]
 print(d(a,b))
 print(np.dot(a,b))
 print(l(a))
 print(np.linalg.norm(a))
+
