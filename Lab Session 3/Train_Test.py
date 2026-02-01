13d56dc6acf540491e0347360c6bb1d9a34b9766
G.S.Akshaya <syamalambaakshaya@gmail.com>
2026-02-01 22:31:50 +0530

    Update Train_Test.py

--- Lab Session 3/Train_Test.py
+++ Lab Session 3/Train_Test.py
@@ -1,4 +1,8 @@
+import pandas as pd
 from sklearn.model_selection import train_test_split
+df=pd.read_excel("Lab Session Data.xlsx",sheet_name="IRCTC_Stock_Price")
+X=df[["Open","High","Low","Volume"]].to_numpy()
+y=(df["Close"].shift(-1)>df["Close"]).astype(int).to_numpy()
+X=X[:-1]
+y=y[:-1]
 x1,x2,y1,y2=train_test_split(X,y,test_size=0.3)
-print(x1.shape)
-print(x2.shape)
