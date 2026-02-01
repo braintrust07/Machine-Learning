69dbdbb0c9916e9e0e82c3b2399594c1e83f9eeb
G.S.Akshaya <syamalambaakshaya@gmail.com>
2026-02-01 22:22:29 +0530

    Update Spread_Distance.py

--- Lab Session 3/Spread_Distance.py
+++ Lab Session 3/Spread_Distance.py
@@ -1,17 +1,16 @@
+import pandas as pd
 import numpy as np
-def m(x):
-    return sum(x)/len(x)
-def v(x):
-    u=m(x)
-    return sum((i-u)**2 for i in x)/len(x)
-def s(x):
-    return v(x)**0.5
-def ms(x):
-    return np.mean(x,axis=0),np.std(x,axis=0)
+df=pd.read_excel("Lab Session Data.xlsx",sheet_name="IRCTC_Stock_Price")
+X=df[["Open","High","Low","Volume"]].to_numpy()
+y=(df["Close"].shift(-1)>df["Close"]).astype(int).to_numpy()
+X=X[:-1]
+y=y[:-1]
 c1=X[y==0]
 c2=X[y==1]
-m1,s1=ms(c1)
-m2,s2=ms(c2)
+m1=np.mean(c1,axis=0)
+s1=np.std(c1,axis=0)
+m2=np.mean(c2,axis=0)
+s2=np.std(c2,axis=0)
 print(m1)
 print(s1)
 print(m2)
